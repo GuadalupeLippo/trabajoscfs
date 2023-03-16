b8bd2f913a9cbfcbd3b6d1fc0d19899f00969fd8
GuadalupeLippo <gualippo@gmail.com>
2023-03-15 21:47:13 -0300

    agregue instrucciones

--- holamundo2.ts
+++ holamundo2.ts
@@ -1,1 +1,18 @@
 console.log("hola mundo de nuevo");
+console.log ("como cocinar fideos");
+console.log("encender la hornalla");
+console.log("agarrar una olla");
+console.log("llenar la olla de agua");
+console.log("llevarla a la hornalla");
+console.log("esperar a que hierva el agua");
+console.log("echar los fideos al agua");
+console.log("esperar a que se cocinen los fideos");
+console.log("agarrar un colador");
+console.log("colar los fideos");
+
+
+
+
+
+
+
