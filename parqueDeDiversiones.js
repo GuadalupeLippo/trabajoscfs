f9b96d01e4293d15e0444b9a077e9f1799e3f353
GuadalupeLippo <gualippo@gmail.com>
2023-03-21 00:48:49 -0300

    realice cambios

--- parqueDeDiversiones.js
+++ parqueDeDiversiones.js
@@ -3,7 +3,7 @@ exports.__esModule = true;
 var rls = require("readline-sync");
 var alturaDeLaPersona = rls.questionInt("Por favor indique su altura expresada en cm: ");
 if (alturaDeLaPersona >= 130) {
-    console.log("Usted tiene la altura apta para subir a la montaña rusa", alturaDeLaPersona);
+    console.log("Usted tiene la altura adecuada para subir a la montaña rusa", alturaDeLaPersona);
 }
 else
     (console.log("Lo sentimos, usted no tiene la altura adecuada para subir a la montaña rusa"));
