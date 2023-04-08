12f9af09db9f83fce215c0fa3f75c55dad9bbfda
GuadalupeLippo <gualippo@gmail.com>
2023-04-08 20:31:47 -0300

    primerafuncion

---
+++ ejercicioTriangulo.js
@@ -0,0 +1,10 @@
+function calcularAreaRectangulo(base, altura) {
+    return (base * altura / 2);
+}
+console.log("el area del triangulo 1 es:", calcularAreaRectangulo(1, 2));
+console.log("el area del triangulo 2 es:", calcularAreaRectangulo(2, 4));
+console.log("el area del triangulo 3 es:", calcularAreaRectangulo(3, 6));
+console.log("el area del triangulo 4 es:", calcularAreaRectangulo(4, 8));
+console.log("el area del triangulo 5 es:", calcularAreaRectangulo(5, 10));
+console.log("el area del triangulo 6 es:", calcularAreaRectangulo(6, 12));
+console.log("el area del triangulo 7 es:", calcularAreaRectangulo(7, 14));
